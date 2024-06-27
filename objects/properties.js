5f8d0e0c845395eb78702aa69eea8a24cef24835
cloud178 <denis-biryukov-13@mail.ru>
2024-06-27 22:47:23 +0300

    add property

---
+++ objects/properties.js
@@ -0,0 +1,11 @@
+const basketball = {
+    country: "usa",
+    league: "nba" 
+}
+
+console.log(basketball.league)
+
+// add property
+basketball.sportsman = "Lebron"
+
+console.log(basketball.sportsman)
