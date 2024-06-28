e118b67026b5ea678275c19328d085752c079b94
cloud178 <denis-biryukov-13@mail.ru>
2024-06-28 14:33:30 +0300

    else if

--- if/if.js
+++ if/if.js
@@ -6,8 +6,12 @@ if (year == 2010) {
     console.log('hello');
 }
 
+year = 2010
+
 if (year == 2011) {
     console.log('hello');
+} else if (year > 2011) {
+    console.log('wow');
 } else {
     console.log(':(')
 }
